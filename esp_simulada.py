3ce089b108976adac0f771e4c354de0097fc94dc
PhilipeSousa <barrosphilipe07@gmail.com>
2025-01-09 17:17:44 -0300

    adiciona cliente mqtt simulando a esp em python

--- esp_simulada.py
+++ esp_simulada.py
@@ -20,3 +20,53 @@ except Exception as e:
 broker = "test.mosquitto.org"
 
 #print(f"Conectando ao broker {broker} na porta {porta} e no tópico {topico}")
+
+# Funções de callback do client mqtt 
+
+def se_conectou(client, userdata, flags, rc):
+    print(f"Conectado com o codigo rc {rc}")
+    client.subscribe(topico)
+
+def nova_mensagem_foi_recebida(client, userdata, msg):
+    print(f"Uma mensagem foi recebida no topico {msg.topico}: {msg.payload.decode()}")
+
+
+
+# Simulando dados vindos de sensores conectados na ESP
+
+def gera_dados_sensores():
+    data = {
+        "Temperatura Ambiente": round(random.uniform(20.0, 40.0), 1),
+        "Temperatura de Água": round(random.uniform(20.0, 45.0),1),
+        "Umidade do ar": round(random.uniform(20, 98),0),
+        "TDS": round(random.uniform(200.0, 1000.0),1),
+        "Bomba": random.choice(["Ligada", "Desligada"]),
+        "LED": random.choice(["Ligada", "Desligada"]),
+    }
+
+    return json.dumps(data)
+
+# Conexao e publica de dados
+
+client = mqtt.Client("ESP Simulada ghuehe")
+client.on_connect = se_conectou
+client.on_messege = nova_mensagem_foi_recebida
+
+client.connect(broker, porta, 60)
+
+
+# Envio de dados
+
+try:
+    while True:
+        dados_dos_sensores = gera_dados_sensores()
+        print(f"Enviando dados: {dados_dos_sensores}")
+        client.publish(topico, dados_dos_sensores)
+
+        # pausa 5 segundos para enviar dnv
+        time.sleep(5)
+except KeyboardInterrupt:
+    print("Conexão encerrada por vc")
+finally:
+    client.disconnect()
+    print("Desconectado do broker")
